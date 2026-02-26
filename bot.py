35abcbcb0a9e76ed138450f1c6dc43775670d82b
lgarciav-cpu <lgarciav@cisco.com>
2026-02-26 10:40:23 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ import glob
 
 # --- NUEVAS LIBRERÍAS DE IA ---
 import google.generativeai as genai
-import chromadb
 import PyPDF2
 # =========================
 # CONFIG
@@ -261,6 +260,7 @@ def ping():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", "5000")))
+
 
 
 
